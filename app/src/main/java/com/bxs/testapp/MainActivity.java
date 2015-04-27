55149c21c67b140b963ddcda8310c0f891df157c
bhupinder <bhupinderjitbawa@gmail.com>
2015-04-27 18:13:30 +0530

    Initial Commit1

--- app/src/main/java/com/bxs/testapp/MainActivity.java
+++ app/src/main/java/com/bxs/testapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String hellp = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
